fb1a70e630166b654f7d06eb3a3897512efe8346
AntonioSalazarB <125318340+AntonioSalazarB@users.noreply.github.com>
2023-06-03 18:15:10 -0600

    subiendo

---
+++ app/src/main/java/com/example/a8botones/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.a8botones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void boton1(View boton1){
+        Toast.makeText(this, "HOLA", Toast.LENGTH_SHORT).show();
+    }
+    public void boton2(View boton2){
+        Toast.makeText(this, "ADIOS", Toast.LENGTH_SHORT).show();
+    }
+    public void boton3(View boton3){
+        Toast.makeText(this, "NUMERO", Toast.LENGTH_SHORT).show();
+    }
+    public void boton4(View boton4){
+        Toast.makeText(this, "BIEN", Toast.LENGTH_SHORT).show();
+    }
+    public void boton5(View boton5){
+        Toast.makeText(this, "DIA", Toast.LENGTH_SHORT).show();
+    }
+    public void boton6(View boton6){
+        Toast.makeText(this, "NOCHE", Toast.LENGTH_SHORT).show();
+    }
+    public void boton7(View boton7){
+        Toast.makeText(this, "BIEN", Toast.LENGTH_SHORT).show();
+    }
+    public void boton8(View boton8){
+        Toast.makeText(this, "FINAL", Toast.LENGTH_SHORT).show();
+    }
+}
