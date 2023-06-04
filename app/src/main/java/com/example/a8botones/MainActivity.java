3244a695e279e494e046f60503271272e9ed8a9b
Ingrid Patricia Martinez Montes <105942832+patimotvel18@users.noreply.github.com>
2023-06-04 15:38:34 -0600

    Actualizar MainActivity.java

--- app/src/main/java/com/example/a8botones/MainActivity.java
+++ app/src/main/java/com/example/a8botones/MainActivity.java
@@ -20,21 +20,21 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "ADIOS", Toast.LENGTH_SHORT).show();
     }
     public void boton3(View boton3){
-        Toast.makeText(this, "NUMERO", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "REAUNUDAR", Toast.LENGTH_SHORT).show();
     }
     public void boton4(View boton4){
-        Toast.makeText(this, "BIEN", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "COMENTA", Toast.LENGTH_SHORT).show();
     }
     public void boton5(View boton5){
-        Toast.makeText(this, "DIA", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "PAUSA", Toast.LENGTH_SHORT).show();
     }
     public void boton6(View boton6){
-        Toast.makeText(this, "NOCHE", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "TERMINAR", Toast.LENGTH_SHORT).show();
     }
     public void boton7(View boton7){
-        Toast.makeText(this, "BIEN", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "CONTINUAR", Toast.LENGTH_SHORT).show();
     }
     public void boton8(View boton8){
-        Toast.makeText(this, "FINAL", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "SALIR", Toast.LENGTH_SHORT).show();
     }
 }
